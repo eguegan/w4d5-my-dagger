d17bcd0ebb82100fd3ddad5dd1ec59c26549ca4e
eguegan <e.guegan@margulls.com>
2016-04-28 17:45:53 -0400

    Cusstom My Dagger

--- app/src/main/java/com/example/admin/w4d5dagger/App.java
+++ app/src/main/java/com/example/admin/w4d5dagger/App.java
@@ -1,9 +1,24 @@
 package com.example.admin.w4d5dagger;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 
 /**
  * Created by admin on 4/28/2016.
  */
 public class App extends Application {
+
+    private SharedComponent mSharedComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mSharedComponent = DaggerSharedComponent.builder().appModule(new AppModule(this)).sharedModule(new SharedModule("Hello")).build();
+
+    }
+
+    public SharedComponent getSharedComponent() {
+        return mSharedComponent;
+    }
 }
--- app/src/main/java/com/example/admin/w4d5dagger/MainActivity.java
+++ app/src/main/java/com/example/admin/w4d5dagger/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.admin.w4d5dagger;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivityTAG_";
+    private static final String SHARED_PREF_KEY = "SHARED_KEY";
+    private static final String SHARED_PREF_VALUE = "SHARED_VALUE";
+    private static final String SHARED_PREF_DEFAULT = "SHARED_DEFAULT";
+
+    @Inject
+    SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((App) getApplication()).getSharedComponent().inject(this);
+
+
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(SHARED_PREF_KEY, SHARED_PREF_VALUE);
+        editor.apply();
     }
 }
